f12988c340df19f61aec7b6d67e3dffb27f35c65
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2017-08-30 15:41:20 +0200

    Implement method loadObject() in AbstractDAO class in same name module

--- AbstractDAO.java
+++ AbstractDAO.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+import java.nio.file.NoSuchFileException;
+
 public abstract class AbstractDAO<T> implements DAOInterface<T> {
+    String path;
+    ArrayList objectsList;
+
     public ArrayList loadObject() {
-        ArrayList objectsList;
-        return objectsList;
+        try (Scanner readFile = new Scanner(new File(this.path))) {
+            this.objectsList.add(readFile.nextLine());
+        } catch (NoSuchFileException e) {
+            System.out.println("This file not exist in choosen directory");
+            e.printStackTrace();
+        }
     }
 
     public void saveObject(T object) {
