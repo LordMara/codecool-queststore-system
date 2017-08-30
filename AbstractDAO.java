fdf9ae79e87f50a49030d5893efa25e2ece125a1
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2017-08-30 15:07:33 +0200

    Add methods loadObject() and saveObject() in class AbstractDAO in module AbstractDAO

---
+++ AbstractDAO.java
@@ -0,0 +1,10 @@
+public abstract class AbstractDAO<T> implements DAOInterface<T> {
+    public ArrayList loadObject() {
+        ArrayList objectsList;
+        return objectsList;
+    }
+
+    public void saveObject(T object) {
+
+    }
+}
