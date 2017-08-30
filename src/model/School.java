bd059be6d7577c456bf0aaa4335a991a6714af83
wereneke <weronikad752@gmail.com>
2017-08-30 14:38:55 +0200

    classes frame

--- src/model/School.java
+++ src/model/School.java
@@ -1,6 +1,23 @@
+package src.model;
+
+import java.util.ArrayList;
+
 public class School {
 
+    ArrayList<User> persons;
+    ArrayList<Class> classes;
+
     public School() {
-        
+
+    }
+
+    private ArrayList<User> loadUsers(String filePath) {
+
+
+    }
+
+    private User createUser(String userLine) {
+
+
     }
 }
