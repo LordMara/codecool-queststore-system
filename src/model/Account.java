9d3ddef45eb4f38425dc08dccaabd358f05c929a
KonradLekawski <Snajperv52@gmail.com>
2017-08-30 14:19:03 +0200

    add getters and setters into Account class

---
+++ src/model/Account.java
@@ -0,0 +1,24 @@
+package src.model;
+
+public abstract class Account{
+
+    protected String name;
+    protected String email;
+    protected String phone;
+    protected String password;
+    protected Integer ID;
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+
+    public 
+}
