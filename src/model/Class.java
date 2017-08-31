bb6230f94b32a26c70a40bd74a357c7e8b9ff77d
wereneke <weronikad752@gmail.com>
2017-08-31 11:27:17 +0200

    class frame

--- src/model/Class.java
+++ src/model/Class.java
@@ -1,5 +1,31 @@
 package src.model;
 
+import java.util.ArrayList;
+
 public class Class {
+
+    Mentor mentor;
+    ArrayList<Student> students;
+    public Class(Mentor newMentor, ArrayList<Student> newStudents) {
+
+        mentor = newMentor;
+        students = newStudents;
+    }
+
+    public Mentor getMentor() {
+        return mentor;
+    }
+
+    public ArrayList<Student> getStudents() {
+        return students;
+    }
+
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+
+    public void replaceMentor(Mentor newMentor) {
+        mentor = newMentor;
+    }
 
 }
