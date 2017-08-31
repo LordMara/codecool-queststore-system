1d57642615b03df864f4a12110637588943e9e68
KonradLekawski <Snajperv52@gmail.com>
2017-08-31 20:10:42 +0200

    Fixed :
    * constructor in Administration controller
    * variable names in Administration controller
    * add static getter in Mentor model

--- src/app/Application.java
+++ src/app/Application.java
@@ -8,9 +8,11 @@ class Application{
 
     public static void main(String[] args){
         MentorController ctrl = new MentorController();
-
+        AdministratorController controller = new AdministratorController();
+        controller.createMentor();
         ctrl.createStudent();
         ctrl.createStudent();
         ctrl.showAllStudents();
+        controller.showAllMentors();
     }
 }
--- src/controller/AdministratorController.java
+++ src/controller/AdministratorController.java
@@ -7,28 +7,29 @@ import src.view.*;
 
 public class AdministratorController{
 
-    AdministratorController(){
-        View<Mentor>  view = new View();
+    View <Mentor> view;
+
+    public AdministratorController(){
+        this.view = new View();
     }
 
-    public void createMantor(){
-        String name = viev.getStringInput("Enter mentor's name: ");
-        String surname = viev.getStringInput("Enter mentor's surname: ");
-        String login = viev.getStringInput("Enter mentor's login: ");
-        String password = viev.getStringInput("Enter mentor's password: ");
+    public void createMentor(){
+        String name = view.getStringInput("Enter mentor's name: ");
+        String surname = view.getStringInput("Enter mentor's surname: ");
+        String login = view.getStringInput("Enter mentor's login: ");
+        String password = view.getStringInput("Enter mentor's password: ");
 
         Mentor mentor = new Mentor(name, surname, login, password);
     }
 
     public void editMentor(Mentor mentor){
-        mentor.setName(Viev.getStringInput("Enter new mentor's name: "));
-        mentor.setSurname(Viev.getStringInput("Enter new mentor's surname: "));
-        mentor.setLogin(Viev.getStringInput("Enter new mentor's login: "));
-        mentor.setPassword(Viev.getStringInput("Enter new mentor's password: "));
-
+        mentor.setName(view.getStringInput("Enter new mentor's name: "));
+        mentor.setSurname(view.getStringInput("Enter new mentor's surname: "));
+        mentor.setLogin(view.getStringInput("Enter new mentor's login: "));
+        mentor.setPassword(view.getStringInput("Enter new mentor's password: "));
     }
 
-    public void showAllStudents(){
-        viev.showAll(Mentor.mentor);
+    public void showAllMentors(){
+        view.showAll(Mentor.getMentors());
     }
 }
