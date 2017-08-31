ebf6de0d4247cca6b111abc093736f4099a0d4b6
KonradLekawski <Snajperv52@gmail.com>
2017-08-31 18:52:28 +0200

    fixing in progress
    * fixed wrong names
    * fixed wrong packages

--- src/model/Student.java
+++ src/model/Student.java
@@ -5,9 +5,9 @@ import java.util.Random;
 
 public class Student extends Account{
 
-    private CodecoolClass codecoolClass;
+    // private CodecoolClass codecoolClass;
     // private Team team;  TODO
-    private Wallet wallet;
+    // private Wallet wallet;
     // private Level level; TODO
 
     private static ArrayList<Student> students = new ArrayList<>();
@@ -16,12 +16,12 @@ public class Student extends Account{
     public Student(){
     }
 
-    public Student(String name, String surname, String login, String password, CodecoolClass codecoolClass){
+    public Student(String name, String surname, String login, String password){
         this.name = name;
         this.surname = surname;
         this.login = login;
         this.password = password;
-        this.codecoolClass = codecoolClass;
+        // this.codecoolClass = codecoolClass;
         generateId();
         this.wallet = null;
         // this.level = new Level(); TODO
