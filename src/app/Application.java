679ff2a1222616dffe664703492000646ac8f9bd
KonradLekawski <Snajperv52@gmail.com>
2017-08-31 23:32:22 +0200

    add menu in app

--- src/app/Application.java
+++ src/app/Application.java
@@ -8,10 +8,40 @@ class Application{
 
     public static void main(String[] args){
 
-        // MentorController mentor = new MentorController();
-        // mentor.startController();
-        AdministratorController admin = new AdministratorController();
-        admin.startController();
+        Application app = new Application();
+        app.start();
     }
 
+    private void start(){
+
+        final String MENTOR_CONTROLLER = "1";
+        final String ADMIN_CONTROLLER = "2";
+        final String EXIT = "0";
+
+        String choose = "";
+        String[] menu = {"Mentor ", "Admin"};
+        AdministratorController admin = new AdministratorController();
+        MentorController mentor = new MentorController();
+        View view = new View();
+
+        while (! choose.equals("0")){
+
+            view.showMenu(menu);
+            choose = view.getStringInput("");
+
+            switch (choose){
+
+                case MENTOR_CONTROLLER :
+                    mentor.startController();
+                    break;
+
+                case ADMIN_CONTROLLER :
+                    admin.startController();
+                    break;
+
+                case EXIT :
+                    break;
+            }
+        }
+    }
 }
