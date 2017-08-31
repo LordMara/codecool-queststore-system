826d42a37dc9ce2abbaf1c2a61150fce824bc04a
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2017-08-31 10:50:45 +0200

    Implement methods load() and save() as abstract, implement getAllObjects(), addObject(), removeObject() in AbstractDAO class

--- DAOInterface.java
+++ DAOInterface.java
@@ -1,4 +1,8 @@
 public interface DAOInterface<T> {
     public void loadObject();
-    public void saveObject(T object);
+    public ArrayList<T> getAllObjects();
+    public void addObject(T object);
+    public boolean removeObject(T object);
+    public void saveObject();
+
 }
