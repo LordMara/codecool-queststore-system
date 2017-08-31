9e1dc13c6dc5c1acf38e5ff333cb442dab1dd252
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2017-08-31 15:31:37 +0200

    Remove methods load() and save() from DAOInterface

--- src/dao/DAOInterface.java
+++ src/dao/DAOInterface.java
@@ -1,10 +1,7 @@
 package src.dao;
 
 public interface DAOInterface<T> {
-    public void loadObject();
     public ArrayList<T> getAllObjects();
     public void addObject(T object);
     public boolean removeObject(T object);
-    public void saveObject();
-
 }
