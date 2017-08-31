a4a11ac4a4440346f358068fb36fa882d9eddefc
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2017-08-31 17:46:37 +0200

    Change parameter in removeObject()

--- src/dao/DAOInterface.java
+++ src/dao/DAOInterface.java
@@ -5,5 +5,5 @@ import java.util.ArrayList;
 public interface DAOInterface<T> {
     public ArrayList<T> getAllObjects();
     public void addObject(T object);
-    public boolean removeObject(T object);
+    public boolean removeObject(Integer id);
 }
