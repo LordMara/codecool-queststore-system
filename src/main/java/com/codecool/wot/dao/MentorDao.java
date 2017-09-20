81934e4cf83e4b5dd48cbd5c3a12c64212c9a730
KonradLekawski <Snajperv52@gmail.com>
2017-09-20 12:00:39 +0200

    implement addObject and removeObject in MentorDao

--- src/main/java/com/codecool/wot/dao/MentorDao.java
+++ src/main/java/com/codecool/wot/dao/MentorDao.java
@@ -1,4 +1,18 @@
 package com.codecool.wot.dao;
 
-public class MentorDao {
+import java.util.ArrayList;
+
+public class MentorDao <Mentor> extends DAOInterface <Mentor> {
+
+    public ArrayList<Mentor> getAllObjects() {
+        return null;
+    }
+
+    public void addObject(Mentor object) {
+
+    }
+
+    public boolean removeObject(Integer id) {
+        return false;
+    }
 }
