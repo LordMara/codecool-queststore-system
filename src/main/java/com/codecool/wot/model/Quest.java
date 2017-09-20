285428c625ce09db716818c66dfdbd8c15720d0b
KonradLekawski <snajperv52@gmail.com>
2017-09-20 22:26:18 +0200

    implement  QUEST DAO into constructor in quest model

--- src/main/java/com/codecool/wot/model/Quest.java
+++ src/main/java/com/codecool/wot/model/Quest.java
@@ -1,13 +1,16 @@
 package com.codecool.wot.model;
 
+import com.codecool.wot.dao.QuestDAO;
+
 public class Quest {
 
     private String name;
     private String description;
 
 
-    public Quest(String name, String description) {
+    public Quest(String name, String description, QuestDAO questDAO) {
         this.name = name;
         this.description = description;
+        questDAO.add(this);
     }
 }
