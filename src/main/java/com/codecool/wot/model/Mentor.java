1920d37e5a82a7c48803a3b64fc0eeb07d44b366
KonradLekawski <Snajperv52@gmail.com>
2017-09-20 17:46:56 +0200

    add add email field into Mentor Model constructor

--- src/main/java/com/codecool/wot/model/Mentor.java
+++ src/main/java/com/codecool/wot/model/Mentor.java
@@ -9,8 +9,9 @@ public class Mentor extends Account{
 
     private SchoolClass schoolClass;
 
-    public Mentor(String name, String surname, String login, String password) {
-        super(name, surname, login, password);
+    public Mentor(String name, String surname, String email, String login, String password, ) {
+        super(name, surname, email, login, password);
+        this.schoolClass = null;
     }
 
     public void setSchoolClass(SchoolClass schoolClass){
@@ -21,8 +22,5 @@ public class Mentor extends Account{
          return this.schoolClass;
      }
 
-    public String toString(){
-        return String.format("Name : %s  | Surname : %s ", this.name, this.surname);
-    }
 
 }
