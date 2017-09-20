dbb49aa90e6531943e9027b87556e3de69eda4a9
KonradLekawski <Snajperv52@gmail.com>
2017-09-20 22:15:41 +0200

    implement Qest class constructor

---
+++ src/main/java/com/codecool/wot/model/Quest.java
@@ -0,0 +1,13 @@
+package com.codecool.wot.model;
+
+public class Quest {
+
+    private String name;
+    private String description;
+
+
+    public Quest(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+}
