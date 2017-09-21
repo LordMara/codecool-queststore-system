75084cf056a0bc4257eed68e15e2f4e1c9a49335
KonradLekawski <Snajperv52@gmail.com>
2017-09-21 15:30:20 +0200

    change class id field type in Student model

--- src/main/java/com/codecool/wot/model/Student.java
+++ src/main/java/com/codecool/wot/model/Student.java
@@ -8,54 +8,20 @@ import java.util.Random;
 
 public class Student extends Account{
 
-     private SchoolClass schoolClass;
-     private Team team;
+    private Integer classId;
+//        private Team team;
     // private Wallet wallet;
     // private Level level; TODO
 
-    public Student(String name, String surname, String email, String login, String password, StudentDAO studentDAO, SchoolClass schoolClass) {
+    public Student(String name, String surname, String email, String login, String password, StudentDAO studentDAO, Integer classId) {
         super(name, surname, email, login, password);
-        this.schoolClass = schoolClass;
+        this.classId = classId;
         studentDAO.add(this);
     }
 
-    public Student(String name, String surname, String email, String login, String password, Integer ID) {
+    public Student(String name, String surname, String email, String login, String password, Integer ID, Integer classId) {
         super(name, surname, email, login, password, ID);
+        this.classId = classId;
     }
-
-    private void addToTeam(Team team) {
-        if (this.ID.equals()) {
-            this.team = team;
-        }
-    }
-
-    public SchoolClass getSchoolClass() {
-        return schoolClass;
-    }
-
-    public Team getTeam() {
-        return team;
-    }
-
-    // public CodecoolClass getSchoolClass(){
-    //     return this.schoolClass;
-    // }
-    //
-    // public void setSchoolClass( SchoolClass schoolClass){
-    //     this.schoolClass = schoolClass;
-    // }
-
-    // public void setTeam(Team team){
-    //     this.team = team; TODO
-    // }
-
-    // public Team getTeam(){
-    //     return this.team; TODO
-    // }
-
-    // public Level getLevel(){
-    //     return this.level;TODO
-    // }
-
 
 }
