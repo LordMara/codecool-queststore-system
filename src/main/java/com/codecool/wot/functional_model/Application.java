ccc252f55f3bcfecbb4531fedb58ad6d3cf1dced
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2017-09-21 11:10:53 +0200

    Implement main() method

--- src/main/java/com/codecool/wot/functional_model/Application.java
+++ src/main/java/com/codecool/wot/functional_model/Application.java
@@ -1,4 +1,9 @@
 package com.codecool.wot.functional_model;
 
 public class Application {
+    public static void main(String[] args) {
+        Controller controller = new Controller();
+
+        controller.startController();
+    }
 }
