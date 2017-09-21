7989bc56d6292d66558021b5404440b17fc9b61b
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2017-09-22 01:34:21 +0200

    Add placeholder for StudentController

--- src/main/java/com/codecool/wot/controller/StudentController.java
+++ src/main/java/com/codecool/wot/controller/StudentController.java
@@ -1,7 +1,10 @@
 package com.codecool.wot.controller;
 
+import com.codecool.wot.view.View;
+
 public class StudentController {
     public void startController() {
-
+        View view = new View();
+        view.printMessage("Under development");
     }
 }
