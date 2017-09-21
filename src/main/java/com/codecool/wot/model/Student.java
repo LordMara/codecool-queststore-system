7b5b06297fdbed2b07cdee35a447d7e45b596a3f
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2017-09-21 15:05:49 +0200

    Change Student constructor

--- src/main/java/com/codecool/wot/model/Student.java
+++ src/main/java/com/codecool/wot/model/Student.java
@@ -4,8 +4,8 @@ import com.codecool.wot.dao.StudentDAO;
 
 public class Student extends Account{
 
-     private Integer codecoolClassId;
-     private Integer teamId;
+     private Integer classId;
+//     private Integer teamId;
 
     public Student(String name, String surname, String email, String login, String password, StudentDAO studentDAO) {
         super(name, surname, email, login, password);
@@ -14,29 +14,28 @@ public class Student extends Account{
     }
 
     public Student(Integer ID, String name, String surname, String email, String login, String password,
-                   StudentDAO studentDAO, Integer codecoolClassId, Integer teamId) {
+                   StudentDAO studentDAO, Integer classId) {
 
-        super(name, surname, email, login, password);
-        this.ID = ID;
-        this.codecoolClassId = codecoolClassId;
-        this.teamId = teamId;
+        super(name, surname, email, login, password, ID);
+        this.classId = classId;
+//        this.teamId = teamId;
 
         studentDAO.add(this);
     }
 
-    public Integer getCodecoolClassId() {
-        return codecoolClassId;
+    public Integer getClassId() {
+        return this.classId;
     }
 
-    public void setCodecoolClassId(Integer codecoolClassId) {
-        this.codecoolClassId = codecoolClassId;
+    public void setClassId(Integer classId) {
+        this.classId = classId;
     }
-
-    public Integer getTeamId() {
-        return teamId;
-    }
-
-    public void setTeamId(Integer teamId) {
-        this.teamId = teamId;
-    }
+//
+//    public Integer getTeamId() {
+//        return teamId;
+//    }
+//
+//    public void setTeamId(Integer teamId) {
+//        this.teamId = teamId;
+//    }
 }
