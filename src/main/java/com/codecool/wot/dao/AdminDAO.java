eea6574557b5538d34f92c349c0daa0a2f552873
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2017-09-21 23:17:03 +0200

    Fix class declaration

--- src/main/java/com/codecool/wot/dao/AdminDAO.java
+++ src/main/java/com/codecool/wot/dao/AdminDAO.java
@@ -3,7 +3,7 @@ package com.codecool.wot.dao;
 import com.codecool.wot.model.Admin;
 import java.sql.*;
 
-public class AdminDAO extends AbstractDAO<Admin> {
+public class AdminDAO extends AbstractCodecoolerDAO<Admin> {
 
 
 
