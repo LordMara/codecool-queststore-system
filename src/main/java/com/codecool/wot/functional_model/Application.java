4de071d3ec1d67084ec6d77d6ed83c6433a28c46
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2017-09-21 13:51:49 +0200

    Add Controller import

--- src/main/java/com/codecool/wot/functional_model/Application.java
+++ src/main/java/com/codecool/wot/functional_model/Application.java
@@ -1,5 +1,7 @@
 package com.codecool.wot.functional_model;
 
+import com.codecool.wot.functional_model.Controller.Controller;
+
 public class Application {
     public static void main(String[] args) {
         Controller controller = new Controller();
