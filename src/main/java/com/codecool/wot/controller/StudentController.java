bf7a19fb642ab58ede8dc3085267285c82ed0a4c
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2017-09-22 03:31:56 +0200

    Add menu blueprint for adding features

--- src/main/java/com/codecool/wot/controller/StudentController.java
+++ src/main/java/com/codecool/wot/controller/StudentController.java
@@ -5,6 +5,39 @@ import com.codecool.wot.view.View;
 public class StudentController {
     public void startController() {
         View view = new View();
-        view.printMessage("Under development");
+
+
+        final String EXIT = "0";
+        final String CHECK_WALLET = "1";
+        final String ENTER_SHOP = "2";
+        final String CHECK_EXPERIENCE = "3";
+
+        String choose = "";
+        String[] menu = {"Check wallet", "Enter shop", "Check experience", };
+
+        while (! choose.equals("0")){
+
+            view.showMenu("Student Menu", menu, "EXIT ");
+            choose = view.getStringInput("");
+
+            switch (choose){
+
+                case CHECK_WALLET :
+                    view.printMessage("Feature in development");
+                    break;
+
+                case ENTER_SHOP :
+                    view.printMessage("Feature in development");
+                    break;
+
+                case CHECK_EXPERIENCE :
+                    view.printMessage("Feature in development");
+                    break;
+
+                case EXIT :
+                    break;
+            }
+        }
     }
 }
+
