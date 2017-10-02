92ef08af223b0e42a2c315ee21ee7f5528caee64
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2017-10-02 13:31:58 +0200

    Add try-with-resources to close connection to database and change logic for validate login of user

--- src/main/java/com/codecool/wot/controller/CentralController.java
+++ src/main/java/com/codecool/wot/controller/CentralController.java
@@ -6,54 +6,52 @@ import com.codecool.wot.model.Admin;
 import com.codecool.wot.dao.*;
 import com.codecool.wot.view.View;
 import java.sql.Connection;
+import java.sql.SQLException;
 
 public class CentralController {
 
-    public void startController() {
+    public void startController() throws SQLException {
 
-        Connection connection = DatabaseConnection.getDBConnection().getConnection();
+        try (Connection connection = DatabaseConnection.getDBConnection().getConnection()){
+            ArtifactDAO arDAO = new ArtifactDAO();
+            QuestDAO qDAO = new QuestDAO();
+            ClassDAO cDAO = new ClassDAO(connection);
 
-        ArtifactDAO arDAO = new ArtifactDAO();
-        QuestDAO qDAO = new QuestDAO();
-        ClassDAO cDAO = new ClassDAO(connection);
+            AdminDAO aDAO = new AdminDAO(connection);
+            MentorDAO mDAO = new MentorDAO(connection);
+            StudentDAO sDAO = new StudentDAO(connection);
 
-        AdminDAO aDAO = new AdminDAO(connection);
-        MentorDAO mDAO = new MentorDAO(connection);
-        StudentDAO sDAO = new StudentDAO(connection);
+            View view = new View();
 
-        View view = new View();
+            String login = view.getStringInput("Enter login");
+            String password = view.getStringInput("Enter password");
 
-        String login = view.getStringInput("Enter login");
-        String password = view.getStringInput("Enter password");
-
-        try {
             Admin admin = aDAO.getByLogin(login);
-            if (password.equals(admin.getPassword())) {
-                AdministratorController adminController = new AdministratorController(mDAO);
-                adminController.startController();
-            }
-        } catch (NullPointerException e1) {
+            Mentor mentor = mDAO.getByLogin(login);
+            Student student = sDAO.getByLogin(login);
 
-            try {
-                Mentor mentor = mDAO.getByLogin(login);
+            if (admin != null) {
+                if (password.equals(admin.getPassword())) {
+                    AdministratorController adminController = new AdministratorController(mDAO);
+                    adminController.startController();
+                }
+            } else if (mentor != null) {
                 if (password.equals(mentor.getPassword())) {
                     MentorController mentorController = new MentorController(cDAO, sDAO, mDAO, qDAO);
                     mentorController.startController();
                 }
-            } catch (NullPointerException e2) {
-
-                try {
-                    Student student = sDAO.getByLogin(login);
-                    if (password.equals(student.getPassword())) {
-
-                        // add connection
-                        StudentController studentController = new StudentController();
-                        studentController.startController();
-                    }
-                } catch (NullPointerException e3) {
-                    view.printMessage("No such user");
+            } else if (student != null) {
+                if (password.equals(student.getPassword())) {
+                    // add connection
+                    StudentController studentController = new StudentController();
+                    studentController.startController();
                 }
+            } else {
+                view.printMessage("No such user");
             }
         }
+
+
+
     }
 }
