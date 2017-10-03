5da8b26213abef8abdb556664112d46813f536ce
KonradLekawski <Snajperv52@gmail.com>
2017-10-03 11:48:40 +0200

    resources directory

--- src/main/java/com/codecool/wot/model/Mentor.java
+++ src/main/java/com/codecool/wot/model/Mentor.java
@@ -4,10 +4,6 @@ public class Mentor extends Account{
 
     private String classId;
 
-    public Mentor(String name, String surname, String email, String login, String password, String ID) {
-        super(name, surname, email, login, password, ID);
-    }
-
     public Mentor(String name, String surname, String email, String login, String password, String ID, String classId) {
         super(name, surname, email, login, password, ID);
         this.classId = classId;
