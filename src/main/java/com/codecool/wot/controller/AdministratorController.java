ea6f567c96943eb8e2b92a31a27b3980c92cf595
KonradLekawski <Snajperv52@gmail.com>
2017-10-03 11:48:09 +0200

    rebuild logic that Admin can create class

--- src/main/java/com/codecool/wot/controller/AdministratorController.java
+++ src/main/java/com/codecool/wot/controller/AdministratorController.java
@@ -1,6 +1,7 @@
 package com.codecool.wot.controller;
 
 
+import com.codecool.wot.dao.ClassDAO;
 import com.codecool.wot.dao.MentorDAO;
 import com.codecool.wot.model.*;
 import com.codecool.wot.view.*;
@@ -9,21 +10,28 @@ public class AdministratorController{
 
     private View <Mentor> view;
     private MentorDAO mentorDAO;
+    private Tools tools;
+    private ClassDAO classDAO;
 
     private final String CREATE_MENTOR = "1";
     private final String EDIT_MENTOR = "2";
     private final String SHOW_MENTORS = "3";
+    private final String CREATE_CLASS = "4";
+    private final String EDIT_CLASS = "5";
     private final String EXIT = "0";
 
-    public AdministratorController(MentorDAO mDAO) {
+    public AdministratorController(MentorDAO mDAO, ClassDAO classDAO, Tools tools) {
         this.view = new View();
         this.mentorDAO = mDAO;
+        this.classDAO = classDAO;
+        this.tools = tools;
+
     }
 
     public void startController(){
 
         String choose = "";
-        String[] menu = {"Create mentor", "Edit mentor", "Show mentor"};
+        String[] menu = {"Create mentor", "Edit mentor", "Create class", "Edit class", "Show mentors"};
 
         while (! choose.equals("0")){
 
@@ -39,6 +47,12 @@ public class AdministratorController{
                 case EDIT_MENTOR :
                     editMentor(getMentorByLogin());
                     break;
+
+                case CREATE_CLASS:
+                    createClass();
+
+                case EDIT_CLASS:
+                    editClass();
 
                 case SHOW_MENTORS :
                     showAllMentors();
@@ -57,7 +71,10 @@ public class AdministratorController{
         String login = view.getStringInput("Enter mentor's login: ");
         String password = view.getStringInput("Enter mentor's password: ");
 
-        mentorDAO.add(new Mentor(name, surname, email, login, password));
+        String classID = classDAO.getID(name);
+        mentorDAO.saveToDataBase(name, surname, email, login, password, classID);
+        String ID = mentorDAO.getID(login);
+        mentorDAO.add(new Mentor(name, surname, email, login, password, ID, classID));
     }
 
     public void editMentor(Mentor mentor){
@@ -82,8 +99,23 @@ public class AdministratorController{
         return mentor;
     }
 
-    private void showAllMentors() {
+    private void createClass() {
 
+        String name = view.getStringInput("Enter class name :");
+
+        classDAO.addToDataBase(name);
+        String classID = classDAO.getId(name);
+
+        classDAO.add(new SchoolClass(name, classID));
+    }
+
+    private void editClass() {
+        SchoolClass schoolClass = tools.getClassByName());
+        schoolClass.setName(view.getStringInput("Enter new class name: "));
+        classDAO.updateClass(schoolClass);
+    }
+
+    private void showAllMentors() {
         view.showAll(mentorDAO.getObjectList());
     }
 }
