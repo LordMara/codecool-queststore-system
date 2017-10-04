97ac3a88a30dec2b11c094ff09b0d0b1c23a1184
JakubJaniszewski <cwanykuba@gmail.com>
2017-10-04 15:35:12 +0200

    Remove tests which should go to AccountTest

--- src/test/java/com/codecool/wot/model/StudentTest.java
+++ src/test/java/com/codecool/wot/model/StudentTest.java
@@ -2,6 +2,7 @@ package com.codecool.wot.model;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Nested;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,54 +17,23 @@ class StudentTest {
     }
 
     @Test
-    public void testIsNameNotNull() {
-        assertNotNull(this.student.getName());
-    }
-
-    @Test
     public void testIsStudentNotNull() {
         assertNotNull(this.student);
     }
 
-    @Test
-    public void testIsNameCorrect() {
-        assertEquals("Paprykarz", this.student.getName());
-    }
 
-    @Test
-    public void testIsEmailCorrect() {
-        assertEquals("szczepa@cc.pl", this.student.getEmail());
-    }
+    @Nested
+    class ClassIDTests {
 
-    @Test
-    public void testIsLoginCorrect() {
-        assertEquals("papaszcze", this.student.getLogin());
-    }
+        @Test
+        public void testIsClassIDCorrect() {
+            assertEquals("3", student.getClassId());
+        }
 
-    @Test
-    public void testIsPasswordCorrect() {
-        assertEquals("papa", this.student.getPassword());
-    }
-
-    @Test
-    public void testIsIDCorrect() {
-        assertEquals("20", this.student.getId());
-    }
-
-    @Test
-    public void testIsClassIDCorrect() {
-        assertEquals("3", this.student.getClassId());
-    }
-
-    @Test
-    public void testIsToStringCorrect() {
-        assertEquals("ID: 20, name: Paprykarz, surname: Szczeciński, email: szczepa@cc.pl, login: papaszcze",
-                     this.student.toString());
-    }
-
-    @Test
-    public void testIsSetClassIDWorking() {
-        this.student.setClassId("5");
-        assertEquals("5", this.student.getClassId());
+        @Test
+        public void testIsSetClassIDWorking() {
+            student.setClassId("5");
+            assertEquals("5", student.getClassId());
+        }
     }
 }
