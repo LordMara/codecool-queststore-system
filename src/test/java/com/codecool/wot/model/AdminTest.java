a51d117ff1314ca5f8bac066a666503c0c5f3376
jakubskiba <jakubskibakrk@gmail.com>
2017-10-04 15:51:32 +0200

    remove redundant tests

--- src/test/java/com/codecool/wot/model/AdminTest.java
+++ src/test/java/com/codecool/wot/model/AdminTest.java
@@ -7,44 +7,14 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AdminTest {
-    
+
     Admin admin;
 
     @BeforeEach
     public void initAdmin() {
         this.admin = new Admin("Jan", "Kowalski", "jkowalski@buziaczek.pl", "jkowal", "haszło", "1");
     }
-
-    @Test
-    public void testToString() {
-        assertEquals("Name : Jan  | Surname : Kowalski ", this.admin.toString());
-    }
-
-    @Test
-    public void testNameField() {
-        assertEquals("Jan", this.admin.getName());
-    }
-
-    @Test
-    public void testSurnameField() {
-        assertEquals("Kowalski", this.admin.getSurname());
-    }
-
-    @Test
-    public void testEmailField() {
-        assertEquals("jkowalski@buziaczek.pl", this.admin.getEmail());
-    }
-
-    @Test
-    public void testLoginField() {
-        assertEquals("jkowal", this.admin.getLogin());
-    }
-
-    @Test
-    public void testPasswordField() {
-        assertEquals("haszło", this.admin.getPassword());
-    }
-
+    
     @Test
     public void testAdminIsNotNull() {
         assertNotNull(this.admin);
