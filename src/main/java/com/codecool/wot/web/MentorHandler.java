bee8ad13c93ad6af397f9c790fb425fb0b84a3e5
KonradLekawski <Snajperv52@gmail.com>
2017-11-16 17:06:54 +0100

    merge

--- src/main/java/com/codecool/wot/web/MentorHandler.java
+++ src/main/java/com/codecool/wot/web/MentorHandler.java
@@ -4,6 +4,8 @@ import com.codecool.wot.dao.*;
 import com.codecool.wot.model.Mentor;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import org.jtwig.JtwigModel;
+import org.jtwig.JtwigTemplate;
 
 import java.io.IOException;
 import java.io.OutputStream;
@@ -23,20 +25,18 @@ public class MentorHandler implements HttpHandler {
         Mentor mentor = mentorDAO.getById(userId);
 
         if (mentor != null && Integer.toString(userId).equals(parseURIToGetId(uri.getPath()))) {
-            String response = "Hello Mentor";
-            httpExchange.sendResponseHeaders(200, response.length());
+            JtwigTemplate template = JtwigTemplate.classpathTemplate("templates/mentor.html");
+            JtwigModel model = JtwigModel.newModel();
+            model.with("name", mentor.getName());
+
+            String response = template.render(model);
+            httpExchange.sendResponseHeaders(200, response.getBytes().length);
             OutputStream os = httpExchange.getResponseBody();
             os.write(response.getBytes());
             os.close();
         } else {
             handleWrongUser(httpExchange);
         }
-
-        String response = "Hello Mentor";
-        httpExchange.sendResponseHeaders(200, response.length());
-        OutputStream os = httpExchange.getResponseBody();
-        os.write(response.getBytes());
-        os.close();
     }
 
     private void handleWrongUser(HttpExchange httpExchange) throws IOException {
