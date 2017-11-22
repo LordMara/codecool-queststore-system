90cf8e2e6e87077e7325d83251e665069dcaa1a0
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2017-11-22 15:43:55 +0100

    Fix bug with wrong database name

--- src/main/java/com/codecool/wot/dao/DatabaseMigration.java
+++ src/main/java/com/codecool/wot/dao/DatabaseMigration.java
@@ -6,7 +6,7 @@ public class DatabaseMigration {
 
     public static void migrateDatabase() {
         Flyway flyway = new Flyway();
-        flyway.setDataSource("jdbc:sqlite:src/main/resources/db/database.db", null, null);
+        flyway.setDataSource("jdbc:sqlite:src/main/resources/db/queststore.db", null, null);
         flyway.migrate();
     }
 
