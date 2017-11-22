a91bcd0926d21bfd58cb3f4860b4cff9f828ae9d
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2017-11-22 15:21:00 +0100

    Add call of method to migrate database at server start

--- src/main/java/com/codecool/wot/Application.java
+++ src/main/java/com/codecool/wot/Application.java
@@ -1,5 +1,6 @@
 package com.codecool.wot;
 
+import com.codecool.wot.dao.DatabaseMigration;
 import com.codecool.wot.web.*;
 //import com.codecool.wot.web.Cookie;
 import com.sun.net.httpserver.HttpServer;
@@ -7,6 +8,8 @@ import java.net.InetSocketAddress;
 
 class Application {
     public static void main(String[] args) throws Exception {
+        // migrate database at server start
+        DatabaseMigration.migrateDatabase();
         // create a server on port 8000
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
 
