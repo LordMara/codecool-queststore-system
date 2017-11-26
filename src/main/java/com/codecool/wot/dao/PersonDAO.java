621d1cd93d01d1b3cfdc2e9922fd82703735e71a
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2017-11-26 13:37:12 +0100

    Update remove() method with remove Account object from memory

--- src/main/java/com/codecool/wot/dao/PersonDAO.java
+++ src/main/java/com/codecool/wot/dao/PersonDAO.java
@@ -47,6 +47,8 @@ public class PersonDAO {
             con.setAutoCommit(false);
             ps.executeUpdate();
             con.commit();
+
+            persons.remove(person);
         }
     }
 
