0b7bca0734d9c16ab38160659b52ea05e4dd0d7e
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2017-11-26 16:13:48 +0100

    Implement method addPerson() with necessary logic

--- src/main/java/com/codecool/wot/dao/ClassDAO.java
+++ src/main/java/com/codecool/wot/dao/ClassDAO.java
@@ -45,6 +45,16 @@ public class ClassDAO {
         return schoolClass;
     }
 
+    public void addPerson(SchoolClass schoolClass, Account person) {
+        try {
+            addPersonToClassInDatabase(schoolClass, person);
+            schoolClass.assignPerson(person);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+    }
+
     private void loadClassesFromDatabase() {
         try (Connection con = DatabaseConnection.getConnection();
              PreparedStatement ps = createSelectPreparedStatement(con);
@@ -56,7 +66,6 @@ public class ClassDAO {
 
                 classes.add(new SchoolClass(classId, name));
             }
-
         } catch (SQLException e) {
             e.printStackTrace();
             System.exit(0);
@@ -76,7 +85,6 @@ public class ClassDAO {
                 Account person = PersonDAO.getInstance().getPerson(personId);
                 schoolClass.assignPerson(person);
             }
-
         } catch (SQLException e) {
             e.printStackTrace();
             System.exit(0);
@@ -93,6 +101,27 @@ public class ClassDAO {
     private PreparedStatement createSelectPersonsPreparedStatement(Connection con) throws SQLException {
         String query = "SELECT * FROM persons_classes;";
         PreparedStatement ps = con.prepareStatement(query);
+
+        return ps;
+    }
+
+    private void addPersonToClassInDatabase(SchoolClass schoolClass, Account person) throws SQLException {
+        try (Connection con = DatabaseConnection.getConnection();
+             PreparedStatement ps = createAddPersonPreparedStatement(con, schoolClass, person)) {
+            con.setAutoCommit(false);
+            ps.executeUpdate();
+            con.commit();
+        }
+    }
+
+    private PreparedStatement createAddPersonPreparedStatement(Connection con, SchoolClass schoolClass, Account person) throws SQLException {
+        String role = "";
+        String query = "INSERT INTO persons_classes (personId, classId) VALUES (?, ?);";
+
+        PreparedStatement ps = con.prepareStatement(query);
+
+        ps.setInt(1, person.getId());
+        ps.setInt(2, schoolClass.getId());
 
         return ps;
     }
