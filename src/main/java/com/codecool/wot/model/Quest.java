e0743d09c521be68ee626aaf9d3b6d59aca36d24
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2017-11-26 17:43:28 +0100

    Update Quest model with setters, getters and clean code fixes

--- src/main/java/com/codecool/wot/model/Quest.java
+++ src/main/java/com/codecool/wot/model/Quest.java
@@ -1,25 +1,59 @@
 package com.codecool.wot.model;
 
-import com.codecool.wot.dao.QuestDAO;
-
 public class Quest {
-
+    private static Integer lastId = 1;
+    private Integer id;
     private String name;
     private String description;
-    private Integer ID;
-    private Float price;
-    private static Integer lastID = 1;
+    private Double price;
 
-    public Quest(String name, String description, Float price) {
+    public Quest(String name, String description, Double price) {
+        this.id = ++lastId;
         this.name = name;
         this.description = description;
-        generateID();
         this.price = price;
     }
 
-    private void generateID() {
-        this.ID = Quest.lastID;
-        Quest.lastID++;
+    public Quest(Integer id, String name, String description, Double price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
 
+        if(lastId < id) {
+            lastId = id;
+        }
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
     }
 }
