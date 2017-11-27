566f4750adb5c2e1602635f1c0cdc56dad4b127c
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2017-11-27 09:11:32 +0100

    Add comments what to do in future

--- src/main/java/com/codecool/wot/dao/PersonDAO.java
+++ src/main/java/com/codecool/wot/dao/PersonDAO.java
@@ -201,5 +201,7 @@ public class PersonDAO {
     private void removeFromAplication(Account person) {
         Codecooler codecooler = (Codecooler) person;
         ClassDAO.getInstance().removePerson(codecooler.getSchoolClass(), person);
+        // call BillDAO to remove all position with this Account
+        // call WalletDAO to remove wallet with this Account
     }
 }
