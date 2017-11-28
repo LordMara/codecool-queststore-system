952124f75c09b9188dda2d2e89160b48c52718b0
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2017-11-28 13:57:47 +0100

    Delete artifact

--- src/main/java/com/codecool/wot/model/Mentor.java
+++ src/main/java/com/codecool/wot/model/Mentor.java
@@ -13,7 +13,6 @@ public class Mentor extends Account implements Codecooler {
 
     public Mentor(String name, String surname, String email, String login, String password, Integer ID, Integer classId) {
         super(name, surname, email, login, password, ID);
-        System.out.println(classId);
         if (!classId.equals(0)) {
             this.schoolClass = ClassDAO.getInstance().getClass(classId);
             this.schoolClass.assignPerson(this);
